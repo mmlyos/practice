35fc8daba22b2928c15285f61c477459c19f1676
Poorya <pooryaghayoomi2006@gmail.com>
2025-12-15 20:22:11 +0330

    Initial main function

---
+++ p1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "---Menu---" << endl;
+    cout << "1)Simple calculator" << endl;
+    cout << "2)Root finder" << endl;
+    cout << "3)Advanced calculator" << endl;
+    int n;
+
+    do
+    {
+        cin >> n;
+    } while (n <= 0 || n >= 4);
+    switch (n)
+    {
+    case 1:
+    //Poorya
+        break;
+
+    case 2:
+    //Alireza
+        break;
+
+    case 3:
+    //Mohammad
+        break;
+    }
+
+    return 0;
+}
